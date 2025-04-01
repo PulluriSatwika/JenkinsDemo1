4011bab0a340ac06bac8a79dd2a16f95a6b0ab3e
PulluriSatwika <245122748005@mvsrec.edu.in>
2025-04-01 12:57:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+
+    for(String str: args) {
+      // convert into integer type
+    int argument = Integer.parseInt(str);
+    System.out.println("Argument in integer form: " + argument);
+    }
+
+  }
+}
+
